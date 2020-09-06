788ce7278ac21036e350a389ebc8f8caff3259da
Anthony Dawson <anthonypdawson@gmail.com>
2020-09-06 13:25:25 -0400

    Added player ID, print statements for testing draw

--- games/war.py
+++ games/war.py
@@ -5,9 +5,11 @@ from cards.card import Deck, GameCard
 
 class Player:
     hand: List[GameCard]
+    id : int
 
-    def __init__(self, hand):
+    def __init__(self, hand, id):
         self.hand = hand
+        self.id = id
 
     def draw(self) -> GameCard:
         return self.hand.pop(0)
@@ -29,12 +31,15 @@ class War:
 
     def setup_game(self):
         deck_one, deck_two = self.deck.split_deck()
+        self.deck.cards.clear()
         self.player_one = Player(deck_one)
         self.player_two = Player(deck_two)
 
     def draw(self):
         p1_card = self.player_one.draw()
+        print("Player 1 draws card {}".format(p1_card))
         p2_card = self.player_two.draw()
+        print("Player 2 draws card {}".format(p2_card))
         self.deck.cards += [p1_card, p2_card]
         winner: Player
         if p1_card > p2_card:
@@ -42,8 +47,10 @@ class War:
         elif p1_card < p2_card:
             winner = self.player_two
         else:
+            print("Equal!")
             pass
         if winner:
+            print("{} wins".format(winner))
             for card in self.deck.cards:
                 winner.take(card)
             self.deck.cards.clear()
